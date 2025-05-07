180cdab7cd90fa52a93649f4577e8ae23e267190
xoxin <xoxinweb@gmail.com>
2025-05-07 22:20:25 +0300

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainChatComponent } from './components/main-chat/main-chat.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MainChatComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'TestAI-chat';
 }
---
+++ src/app/components/main-chat/main-chat.component.ts
@@ -0,0 +1,84 @@
+import {
+  Component,
+  inject,
+  OnInit
+} from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
+import { Mistral } from '@mistralai/mistralai';
+import { ContentChunk } from '@mistralai/mistralai/models/components';
+import { API_KEY } from '../../../API_KEY';
+import { LocalService } from '../../services/local-service.service';
+
+@Component({
+  selector: 'app-main-chat',
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
+  templateUrl: './main-chat.component.html',
+  styleUrls: ['./main-chat.component.scss']
+})
+
+export class MainChatComponent implements OnInit{
+  private apiKey = API_KEY.MISTRAL_API_KEY;
+  private client = new Mistral({ apiKey: this.apiKey });
+  private localService = inject(LocalService);
+
+  public messages: { text: string | ContentChunk[] | null, isUser: boolean }[] = [];
+  public userInput = '';
+
+  ngOnInit() {
+    this.messages = JSON.parse(
+      this.localService.getData('chatMessages') || '[]'
+    );
+  }
+
+  public async sendMessage() {
+    const userMessage = this.userInput;
+
+    if (!this.userInput.trim()) return;
+
+    this.addMessageInChat();
+
+    this.userInput = '';
+
+    try {
+      const chatResponse = await this.client.chat.complete({
+        model: 'mistral-large-latest',
+        messages: [{role: 'user', content: userMessage}],
+      });
+
+      if ( chatResponse && chatResponse.choices && chatResponse.choices.length > 0 ) {
+        this.messages.push({
+          text: chatResponse.choices[0].message.content!,
+          isUser: false
+        });
+      }
+    } catch (error) {
+      console.error('Error getting chat response:', error);
+
+      this.messages.push({
+        text: 'Sorry, there was an error processing your request.',
+        isUser: false
+      });
+    }
+
+    this.saveMessagesToLocalStorage();
+  }
+
+  public addMessageInChat() {
+    this.messages.push({ text: this.userInput, isUser: true });
+  }
+
+  public clearChat() {
+    this.messages = [];
+    this.saveMessagesToLocalStorage();
+  }
+
+  public saveMessagesToLocalStorage() {
+    const messagesString = JSON.stringify(this.messages);
+    this.localService.saveData('chatMessages', messagesString);
+  }
+}
